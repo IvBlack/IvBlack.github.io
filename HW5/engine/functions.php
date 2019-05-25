46387a81f9b6b2f8293277b7b1c8d7fc184440f2
IvBlack <49375406+IvBlack@users.noreply.github.com>
2019-05-25 17:54:38 +0300

    update functions.php

--- HW5/engine/functions.php
+++ HW5/engine/functions.php
@@ -33,7 +33,7 @@ function createGallery($imgDir) {
 	$result = '';
 
 	$images = scandir(WWW_DIR . $imgDir);
-
+	var_dump($images);
 
 	foreach ($images as $image) {
 		if(is_file(WWW_DIR . $imgDir . $image)) {
@@ -43,4 +43,22 @@ function createGallery($imgDir) {
 		}
 	}
 	return $result;
+}
+
+function renderGallery($allImages)
+{ 
+
+	$galleryContent = '';
+	foreach ($allImages as $galleryItem) {
+		if (empty($galleryItem['image'])) {
+			$galleryItem['image'] = 'img/no-image.jpeg';
+		}
+			//Делаю выборку нужных значений для шаблона и склеиваю все в одну строку, плюс вырезал из шаблона target blank возврат на страницу галереи по клику на картинку происходит
+		$galleryContent .= render(TEMPLATES_DIR . 'galleryItem.tpl', [
+				'id' => $galleryItem['id'],
+				'src' => $galleryItem['url']
+			]);
+	}
+
+	return $galleryContent;
 }

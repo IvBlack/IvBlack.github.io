f74b039a734ce164322c205db78705f5c6bb426b
IvBlack <49375406+IvBlack@users.noreply.github.com>
2019-05-25 17:31:20 +0300

    update galleryItem.php

--- HW5/public/galleryItem.php
+++ HW5/public/galleryItem.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../config/config.php';
 
 //var_dump($_GET['id']);
+//получаем id
 
 $id = isset($_GET['id']) ? $_GET['id'] : false;
 
@@ -11,6 +12,7 @@ if(!$id) {
 	exit();
 }
 
+//show конектится к бд с запросом $sql и возвращает массив всех полей для элемента с указаным id
 $sql = "SELECT * FROM images WHERE id = $id";
 
 $galleryItem = show($sql);
@@ -20,8 +22,9 @@ if(!$galleryItem) {
 	die;
 }
 
-echo render(TEMPLATES_DIR . 'galleryItem.tpl', [
-	'title' => $galleryItem['title'],
-	'content' => $galleryItem['content'],
-	'h1' => $galleryItem['title']
-]);
+//из этого массива нам нужен только элемент с индексом 'url' а также потребовался другой шаблон -> вывод в полном размере + ссылка на галлерею при клике
+
+echo render(TEMPLATES_DIR . 'galleryItemSolo.tpl', [
+	'id' => $id,
+	'src' => $galleryItem['url']
+	]);
